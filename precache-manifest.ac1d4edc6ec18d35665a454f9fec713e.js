18ab48600fe7894d8939875db95a2d064c3c5e8e
Yogesh Dabral <dabralyogesh@gmail.com>
2020-04-28 22:01:37 +0530

    Updates

---
+++ precache-manifest.ac1d4edc6ec18d35665a454f9fec713e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd3397b15478fc5f971f670f8ca1989",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2f18d7c0f2c910463733",
+    "url": "/robofriends/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "bf477fd2f45dd41a98ff",
+    "url": "/robofriends/static/css/main.8b8376e9.chunk.css"
+  },
+  {
+    "revision": "2f18d7c0f2c910463733",
+    "url": "/robofriends/static/js/2.f68723af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f68723af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf477fd2f45dd41a98ff",
+    "url": "/robofriends/static/js/main.bec4e25c.chunk.js"
+  },
+  {
+    "revision": "dab07940c1980e5aab1e",
+    "url": "/robofriends/static/js/runtime-main.a4afcfec.js"
+  }
+]);
